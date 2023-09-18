1068c66374a2fad694a07819beb4badf9bd489d5
EL MARINI <52750985+elmariniAbdelkarim@users.noreply.github.com>
2023-09-18 10:03:45 +0200

    Update BrowserTest.cs

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -16,9 +16,11 @@ namespace SeleniumNUnitParam
         public void GoogleTest()
         {
             Driver.Navigate().GoToUrl("http://www.google.com");
+            System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("btnG")).Click();
+            System.Threading.Thread.Sleep(5000);
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
                                             "The text selenium doest not exist");
 
@@ -28,10 +30,13 @@ namespace SeleniumNUnitParam
         public void ExecuteAutomationTest()
         {
             Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("Password")).SendKeys("admin");
+            System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("Login")).Submit();
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
                                             "The text selenium doest not exist");
 
